3a882713d671a3f9029e8e3f7d5581f4e0696189
Sam <sam@dev.com>
2024-04-11 17:58:16 +0800

    feat: update test compilers version

--- src/components/Provider/error.js
+++ src/components/Provider/error.js
@@ -1,4 +1,3 @@
-const inherits = require('util').inherits;
 const TruffleError = require('@truffle/error');
 
 // HACK: string comparison seems to be only way to identify being unable to
@@ -19,6 +18,20 @@ function ProviderError(message) {
   this.message = message;
 }
 
-inherits(ProviderError, TruffleError);
+class ProviderError extends TruffleError {
+  constructor(message) {
+    if (message === NOT_CONNECTED_MESSAGE) {
+      message =
+        'Could not connect to your Ethereum client. ' +
+        'Please check that your Ethereum client:\n' +
+        '    - is running\n' +
+        '    - is accepting RPC connections (i.e., "--rpc" option is used in geth)\n' +
+        '    - is accessible over the network\n' +
+        '    - is properly configured in your tronbox configuration file (tronbox.js)\n';
+    }
+    super(message);
+    this.message = message;
+  }
+}
 
 module.exports = ProviderError;
